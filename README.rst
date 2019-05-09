b7e7b926089a8ef38baf4df73e8162d00d255f86
CAyyanar <cayyanar@cccis.com>
2019-05-08 21:29:50 -0500

    updated the comments

---
+++ README.rst
@@ -0,0 +1,20 @@
+===============================
+Sample_email_reader
+===============================
+
+.. image:: https://img.shields.io/travis/acdurai04/email_sample.svg
+        :target: https://travis-ci.org/acdurai04/email_sample
+
+.. image:: https://img.shields.io/pypi/v/email_sample.svg
+        :target: https://pypi.python.org/pypi/email_sample
+
+
+email pacakage validation
+
+* Free software: MIT license
+* Documentation: (COMING SOON!) https://email_sample.readthedocs.org.
+
+Features
+--------
+
+* TODO
--- setup.py
+++ setup.py
@@ -1,74 +1,30 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/guides/distributing-packages-using-setuptools/
-https://github.com/acdurai/email_reader
-"""
-
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from os import path
 from io import open
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+    readme = readme_file.read()
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
+with open(path.join(here, 'HISTORY.rst'), encoding='utf-8') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
 
 setup(
+    name='email_reader',  
+    version='1.0.2',  
+    description='Sample email reader ',  
+    long_description=readme + '\n\n' + history,
+    author='Chelladurai',
+    author_email='acdurai04@gmail.com',
+    url='https://github.com/acdurai/email_reader',
     
-    name='email_reader',  # Required
-
-   
-    version='1.0.0',  # Required
-
-   
-    description='Sample email reader ',  # Optional
-
-    
-    long_description=long_description,  # Optional
-
-    
-    long_description_content_type='text/markdown',  # Optional (see note above)
-
-    # This should be a valid link to your project's main homepage.
-    #
-    # This field corresponds to the "Home-Page" metadata field:
-    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/acdurai/email_reader',  # Optional
-
-    # This should be your name or the name of the organization which owns the
-    # project.
-    author='Chelladurai',  # Optional
-
-    # This should be a valid email address corresponding to the author listed
-    # above.
-    author_email='acdurai04@gmail.com',  # Optional
-
-    # Classifiers help users find your project by categorizing it.
-    #
-    # For a list of valid classifiers, see https://pypi.org/classifiers/
     classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        # These classifiers are *not* checked by 'pip install'. See instead
-        # 'python_requires' below.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
@@ -78,21 +34,7 @@ setup(
         'Programming Language :: Python :: 3.7',
     ],
 
-    # This field adds keywords for your project which will appear on the
-    # project page. What does your project relate to?
-    #
-    # Note that this is a string of words separated by whitespace, not a list.
-    keywords='sample setuptools development',  # Optional
-
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
-
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['cryptography'],  # Optional
+    install_requires=['cryptography'],
 
    
 
